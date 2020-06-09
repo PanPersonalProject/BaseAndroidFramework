0d551c5e9a702d34e170d475489e30875153752b
panqi <panqi@tiange.com>
2020-06-09 17:37:18 +0800

    封装Gson，导入kotson

---
+++ common_lib/src/main/java/pan/lib/common_lib/utils/JsonUtil.kt
@@ -0,0 +1,16 @@
+package pan.lib.common_lib.utils
+
+import com.github.salomonbrys.kotson.fromJson
+import com.google.gson.Gson
+
+/**
+ *
+ * Author:         pan qi
+ * CreateDate:     2020/6/9 14:32
+ * 扩展时参考 https://github.com/SalomonBrys/Kotson
+ */
+fun Any.toJson(): String? = Gson().toJson(this)
+
+
+inline fun <reified T : Any> String.toObject(): T = Gson().fromJson(this)
+

e51510a527054d66442b97f02ebe7dd5a68a8572
panqi <panqi@tiange.com>
2020-06-17 10:12:49 +0800

    扩展Number，支持dp->px,sp->px

---
+++ common_lib/src/main/java/pan/lib/common_lib/utils/DimensionUtil.kt
@@ -0,0 +1,34 @@
+package pan.lib.common_lib.utils
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+/**
+ *
+ * Author:         pan qi
+ * CreateDate:     2020/6/16
+ */
+
+
+/**
+ *dp->px
+ */
+
+val Number.dp: Int
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this.toFloat(),
+        Resources.getSystem().displayMetrics
+    ).toInt()
+
+
+/**
+ *sp->px
+ */
+val Number.sp: Int
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP,
+        this.toFloat(),
+        Resources.getSystem().displayMetrics
+    ).toInt()
+

77dffe8bdfaa2324f75fdbc944209836262aea5c
Pan <1913225356@qq.com>
2020-07-09 09:31:24 +0800

    导入hilt

--- app/src/main/java/pan/lib/baseandroidframework/CustomApplication.kt
+++ app/src/main/java/pan/lib/baseandroidframework/CustomApplication.kt
@@ -1,5 +1,6 @@
 package pan.lib.baseandroidframework
 
+import dagger.hilt.android.HiltAndroidApp
 import pan.lib.common_lib.base.BaseApplication
 
 /**
@@ -7,6 +8,7 @@ import pan.lib.common_lib.base.BaseApplication
  * Author:         pan qi
  * CreateDate:     2020/6/3 15:44
  */
+@HiltAndroidApp
 class CustomApplication : BaseApplication() {
 
     override fun onCreate() {

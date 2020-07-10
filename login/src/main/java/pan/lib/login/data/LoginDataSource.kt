410fb22c65fb77105cff95d188908347e76c6625
Pan <1913225356@qq.com>
2020-07-11 00:18:00 +0800

    LoginActivity demo

---
+++ login/src/main/java/pan/lib/login/data/LoginDataSource.kt
@@ -0,0 +1,25 @@
+package pan.lib.login.data
+
+import pan.lib.login.data.model.LoggedInUser
+import java.io.IOException
+import javax.inject.Inject
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource @Inject constructor() {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}

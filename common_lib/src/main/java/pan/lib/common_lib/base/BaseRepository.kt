a0a94b63161f3fcedd0623c4e3dc8922be7b71e1
panqi <panqi@tiange.com>
2020-07-10 15:22:01 +0800

    BaseRepository.kt

---
+++ common_lib/src/main/java/pan/lib/common_lib/base/BaseRepository.kt
@@ -0,0 +1,8 @@
+package pan.lib.common_lib.base
+
+/**
+ *
+ * Author:         pan qi
+ * CreateDate:     2020/7/10
+ */
+open class BaseRepository
--- test/src/main/java/com/example/test/repository/TestRepository.kt
+++ test/src/main/java/com/example/test/repository/TestRepository.kt
@@ -1,6 +1,7 @@
 package com.example.test.repository
 
 import com.example.test.TestApiService
+import pan.lib.common_lib.base.BaseRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,7 +11,7 @@ import javax.inject.Singleton
  * CreateDate:     2020/6/30
  */
 
-//@Singleton
-class TestRepository @Inject constructor( val testApiService: TestApiService) {
+@Singleton
+class TestRepository @Inject constructor(private val testApiService: TestApiService): BaseRepository() {
     suspend fun testApi() = testApiService.testApi()
 }

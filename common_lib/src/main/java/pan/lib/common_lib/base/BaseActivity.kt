e901bfbd71483e8cd97c1842635857f771366ea6
panqi <panqi@tiange.com>
2020-08-27 17:17:28 +0800

    依赖ImmersionBar,实现沉浸式状态栏

--- common_lib/src/main/java/pan/lib/common_lib/base/BaseActivity.kt
+++ common_lib/src/main/java/pan/lib/common_lib/base/BaseActivity.kt
@@ -2,13 +2,22 @@ package pan.lib.common_lib.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gyf.immersionbar.ImmersionBar
+import pan.lib.common_lib.R
 
 abstract class BaseActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initStatusBar()
+    }
 
+    private fun initStatusBar() {
+        ImmersionBar.with(this)
+            .statusBarColor(R.color.colorPrimary)
+            .keyboardEnable(true)  //解决软键盘与底部输入框冲突问题
+            .init();
     }
 
 
